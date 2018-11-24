ba1b9cc6987d540ed3fdd15cb38e48e07a22d58a
Yhoswar Perez <yhoswarperez@gmail.com>
2018-11-24 17:21:09 -0430

    Modificado el ts y el html para saludo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+let valor:string;
+
+valor=prompt('Ingrese su nombre:','');
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'my-app';
+  nombre=valor;
 }
