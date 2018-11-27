b83c8c24eb9393b0b31acf7f443632f1b0c7298d
Yhoswar Perez <yhoswarperez@gmail.com>
2018-11-27 12:44:20 -0430

    se agrego cabecera footer y ruta de contacto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 	show:boolean=true;
+
 	activo:string="";
+
+	arreglos:Array<any>=[
+	{nombre:'Yhoswar',apellido:'Perez',correo:'yhoswarperez@gmail.com'},
+	{nombre:'Jose',apellido:'Perez',correo:'jose@gmail.com'},
+	{nombre:'Luis',apellido:'Gonzales',correo:'Luis@gmail.com'}
+	]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,16 +7,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { FooterComponent } from './footer/footer.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+const routes: Routes = [
+  { path: 'contacto', component: ContactoComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    FooterComponent
+    FooterComponent,
+    CuerpoComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
