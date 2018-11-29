dca3f8fb2ac8d19a84ce680a3ff637787dba5384
Yhoswar Perez <yhoswarperez@gmail.com>
2018-11-29 16:57:58 -0430

    modificado servicio para ver equipo

--- src/app/equipo/equipo.component.ts
+++ src/app/equipo/equipo.component.ts
@@ -14,25 +14,12 @@ equipo:Array<any>=[];
 
   constructor(private ruta:ActivatedRoute,private _servicio:EquipoService) 
   	{
-  		/*this.ruta.params.subscribe(params=>
+  		this.ruta.params.subscribe(params=>
   		{
   			console.log(params['id']);
   			this.equipo=this._servicio.obtenerUnEquipo(params['id']);
-  		})*/
-      this.obtenruta(this.ruta,this._servicio,this.equipo);
-
+  		})
   	}
-
-    obtenruta(ruta2:ActivatedRoute,_servicio2:EquipoService,equipo:Array<any>)
-    {
-        ruta2.params.subscribe(params=>{
-        console.log(params['id']);
-       console.log(_servicio2.obtenerUnEquipo(params['id']));
-       this.equipo=_servicio2.obtenerUnEquipo(params['id']);
-      }
-      )
-    }
-
   ngOnInit() {
   }
 
